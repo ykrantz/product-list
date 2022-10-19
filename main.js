d10c97759e58d4e5ba6698265e6af3e8631ec33b
ykrantz <ykrantz@gmail.com>
2022-10-19 23:54:52 +0300

    fix bugs + adding product list

--- main.js
+++ main.js
@@ -1,87 +1,202 @@
+const INPUT_TYPES = ["name", "price", "description"];
+const INIT_PRODUCT_LIST = [new Product(0, "item1", "10", "test")];
+
+const inputTypes = INPUT_TYPES;
+
+const productList = INIT_PRODUCT_LIST;
+
 const $app = document.getElementById("app");
-
+// ***********
+// create app:
+// ***********
 createMainAppContainer();
+createAppHeader();
 createInputProductDetails();
+createProductsList();
+//
+//
 
-//
-//
-//
+// ***********
+// defenitions:
+// ***********
 
-// main functions:
+function Product(id, prdName, price, description) {
+  // TODO: check if to make as class
+  this.id = id;
+  this.prdName = prdName;
+  this.price = price;
+  this.description = description;
+}
+
+// ***********
+// create elements functions:
+// ***********
+
 function createMainAppContainer() {
-  //   const $appContainer = document.createElement("div");
-  //   $appContainer.setAttribute("id", "appContainer");
-
-  //   document.createElement("div");
-  //   $appContainer.setAttribute("id", "appContainer");
-
-  //   const $inputProductDetails = document.createElement("div");
-  //   $inputProductDetails.setAttribute("id", "inputProductDetails");
-
-  //   const $productsList = document.createElement("div");
-  //   $productsList.setAttribute("id", "productsList");
-
-  //   const $productDitails = document.createElement("div");
-  //   $productDitails.setAttribute("id", "productDitails");
-
   const $appContainer = elementGenerator("div", "appContainer");
+  //   const mainDiv = ["inputProductDetails", "productsList", "productDitails"];
+  const $appHeader = elementGenerator("div", "appHeader");
   const $inputProductDetails = elementGenerator("div", "inputProductDetails");
   const $productsList = elementGenerator("div", "productsList");
   const $productDitails = elementGenerator("div", "productDitails");
 
-  $appContainer.append($inputProductDetails, $productsList, $productDitails);
+  $appContainer.append(
+    $appHeader,
+    $inputProductDetails,
+    $productsList,
+    $productDitails
+  );
 
   document.getElementById("app").append($appContainer);
-  console.log("created app");
+  console.log("app was created");
 }
 
-// const $productDitails = document.getElementById("productDitails");
+function createAppHeader() {
+  const $appHeader = document.getElementById("appHeader");
+  const $newTitle = elementGenerator(
+    "h1",
+    "mainTitle",
+    "title",
+    "Product List"
+  );
 
-function elementGenerator(
-  type,
-  idName = `${type}-id`,
-  className = `${type}-${idName}`
-) {
-  const $newElement = document.createElement(type);
-  $newElement.setAttribute("id", idName);
-  $newElement.setAttribute("class", className);
-  return $newElement;
+  $appHeader.append($newTitle);
 }
 
 function createInputProductDetails() {
-  const inputTypes = ["name", "price", "description"];
+  //   const inputTypes = ["name", "price", "description"];
 
   for (let i of inputTypes) {
     createInputAndLabel(i, i);
   }
+
+  const $inputProductDetails = document.getElementById("inputProductDetails");
+  const $addButton = buttonGenerator(
+    addProductToList,
+    "Add",
+    "addProduct-button",
+    "btn"
+  );
+  $inputProductDetails.append($addButton);
 }
 
 function createInputAndLabel(inputName, inputLabel) {
   const $inputProductDetails = document.getElementById("inputProductDetails");
 
-  //   const $newLabel = document.createElement("label");
   const $newLabel = elementGenerator(
     "label",
     `${inputLabel}Label`,
-    "productInputLable"
+    "productInputLable",
+    inputLabel
   );
   $newLabel.type = "text";
-  //   $newLabel.className = "productInputLable";
-  $newLabel.innerHTML = inputLabel;
 
-  //   const $newInput = document.createElement("input");
   const $newInput = elementGenerator(
     "input",
     `${inputName}Input`,
     "productInput"
   );
   $newInput.type = "text";
-  //   $newInput.className = "productInput";
-  //   $newInput.id = `${inputName}Input`;
+  $newInput.placeholder = `please fill ${inputName}`;
 
-  const $inputDiv = document.createElement("div");
+  const $inputDiv = elementGenerator(
+    "div",
+    `${inputName}-inputDivId`,
+    "inputDiv"
+  );
   $inputDiv.className = "inputDiv";
   $inputDiv.append($newLabel, $newInput);
 
   $inputProductDetails.append($inputDiv);
+}
+
+function createProductsList() {
+  // TODO: delete existing UL
+
+  const $productListUl = elementGenerator("ul");
+
+  for (let product of productList) {
+    console.log(product, 4);
+    const $newProduct = elementGenerator(
+      "li",
+      `${product?.prdName}`,
+      "productLi",
+      product?.prdName
+    );
+    $productListUl.append($newProduct);
+  }
+
+  const $productsList = document.getElementById("productsList");
+  $productsList.append($productListUl);
+}
+// ***********
+// main functions:
+// ***********
+
+function addProductToList() {
+  //   TODO: check using form to get value and delete
+
+  const $nameInput = document.getElementById("nameInput");
+  const $priceInput = document.getElementById("priceInput");
+  const $descriptionInput = document.getElementById("descriptionInput");
+
+  //TODO:  check valid input
+
+  const newProduct = new Product(
+    productList.length,
+    $nameInput.value,
+    $priceInput.value,
+    $descriptionInput.value
+  );
+  productList.push(newProduct);
+
+  console.log("product was added", newProduct);
+  console.log($nameInput);
+  //   TODO: add single item to list instead of create product list
+  createProductsList();
+  clearInputs();
+}
+
+// ***********
+// side functions:
+// ***********
+
+function clearInputs() {
+  const inputsToClear = document.getElementsByClassName("productInput");
+  for (let $input of inputsToClear) {
+    $input.value = "";
+  }
+}
+
+function buttonGenerator(
+  onClickFunc,
+  innerHTMLText = "",
+  idName = `button-id`,
+  className = `button-${idName}`
+) {
+  const $newButton = elementGenerator(
+    "button",
+    "addProduct-button",
+    className,
+    innerHTMLText
+  );
+  $newButton.addEventListener("click", onClickFunc);
+  console.log("created botion", $newButton);
+  return $newButton;
+}
+
+function elementGenerator(
+  type,
+  idName = `${type}-id`,
+  className = `${type}-${idName}`,
+  innerHTMLText = ""
+) {
+  const $newElement = document.createElement(type);
+
+  $newElement.setAttribute("id", idName);
+
+  $newElement.setAttribute("class", className);
+
+  $newElement.innerHTML = innerHTMLText;
+  return $newElement;
 }
