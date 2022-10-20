7d54457c6ace992b4d42f4f3a0cf019c762ecb48
ykrantz <ykrantz@gmail.com>
2022-10-20 22:39:04 +0300

    adding delete button+fix adding bug+ createing details. input types still as array

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ const $app = document.getElementById("app");
 createMainAppContainer();
 createAppHeader();
 createInputProductDetails();
+// createProductsContainer();
 createProductsList();
+createProductDetails();
 //
 //
 
@@ -34,25 +36,22 @@ function Product(id, prdName, price, description) {
 
 function createMainAppContainer() {
   const $appContainer = elementGenerator("div", "appContainer");
-  //   const mainDiv = ["inputProductDetails", "productsList", "productDitails"];
+  //   const mainDiv = ["inputProductDetails", "productsList", "productDetails"];
   const $appHeader = elementGenerator("div", "appHeader");
   const $inputProductDetails = elementGenerator("div", "inputProductDetails");
+  const $poductContainer = elementGenerator("div", "poductContainer");
   const $productsList = elementGenerator("div", "productsList");
-  const $productDitails = elementGenerator("div", "productDitails");
+  const $productDetails = elementGenerator("div", "productDetails");
+  $poductContainer.append($productsList, $productDetails);
 
-  $appContainer.append(
-    $appHeader,
-    $inputProductDetails,
-    $productsList,
-    $productDitails
-  );
+  $appContainer.append($appHeader, $inputProductDetails, $poductContainer);
 
   document.getElementById("app").append($appContainer);
   console.log("app was created");
 }
 
 function createAppHeader() {
-  const $appHeader = document.getElementById("appHeader");
+  // const $appHeader = document.getElementById("appHeader");
   const $newTitle = elementGenerator(
     "h1",
     "mainTitle",
@@ -60,7 +59,7 @@ function createAppHeader() {
     "Product List"
   );
 
-  $appHeader.append($newTitle);
+  document.getElementById("appHeader").append($newTitle);
 }
 
 function createInputProductDetails() {
@@ -70,19 +69,17 @@ function createInputProductDetails() {
     createInputAndLabel(i, i);
   }
 
-  const $inputProductDetails = document.getElementById("inputProductDetails");
   const $addButton = buttonGenerator(
     addProductToList,
     "Add",
     "addProduct-button",
     "btn"
   );
-  $inputProductDetails.append($addButton);
+  // const $inputProductDetails = document.getElementById("inputProductDetails");
+  document.getElementById("inputProductDetails").append($addButton);
 }
 
 function createInputAndLabel(inputName, inputLabel) {
-  const $inputProductDetails = document.getElementById("inputProductDetails");
-
   const $newLabel = elementGenerator(
     "label",
     `${inputLabel}Label`,
@@ -106,32 +103,156 @@ function createInputAndLabel(inputName, inputLabel) {
   );
   $inputDiv.className = "inputDiv";
   $inputDiv.append($newLabel, $newInput);
-
-  $inputProductDetails.append($inputDiv);
+  // const $inputProductDetails = document.getElementById("inputProductDetails");
+  document.getElementById("inputProductDetails").append($inputDiv);
 }
 
+// function createProductsContainer() {
+//   const $productsContainer = elementGenerator("div", "productsContainer");
+// }
 function createProductsList() {
-  // TODO: delete existing UL
+  const $productListUl = elementGenerator("ul", "productListUl");
 
-  const $productListUl = elementGenerator("ul");
+  // for (let product of productList) {
+  for (let i = 0; i < productList.length; i++) {
+    // console.log(product, 4);
+    // const $newProduct = elementGenerator(
+    //   "li",
+    //   `${product?.prdName}`,
+    //   "productLi",
+    //   product?.prdName
+    // );
+    const $newProduct = createProductLi(productList[i], i);
 
-  for (let product of productList) {
-    console.log(product, 4);
-    const $newProduct = elementGenerator(
-      "li",
-      `${product?.prdName}`,
-      "productLi",
-      product?.prdName
-    );
     $productListUl.append($newProduct);
   }
 
-  const $productsList = document.getElementById("productsList");
-  $productsList.append($productListUl);
+  // const $productsList = document.getElementById("productsList");
+  document.getElementById("productsList").append($productListUl);
+}
+
+function createProductDetails() {
+  const $productName = elementGenerator("p", "productName", "productDetails");
+  const $productPrice = elementGenerator("p", "productPrice", "productDetails");
+  const $productDescription = elementGenerator(
+    "p",
+    "productDescription",
+    "productDetails"
+  );
+
+  // const $productDetails = elementGenerator(
+  //   "div",
+  //   "productDetails",
+  //   "productDetailsDiv"
+  // );
+  document
+    .getElementById("productDetails")
+    .append($productName, $productPrice, $productDescription);
+  console.log("15");
+}
+
+function refreshProductsList() {
+  // TODO: delete existing UL and fix
+  clearProductList();
+  // debugger;
+  const $productListUl = document.getElementById("productListUl");
+
+  // for (let product of productList) {
+  for (let i = 0; i < productList.length; i++) {
+    // console.log(product, 4);
+    // const $newProduct = elementGenerator(
+    //   "li",
+    //   `${product?.prdName}`,
+    //   "productLi",
+    //   product?.prdName
+    // );
+
+    const $newProduct = createProductLi(productList[i], i);
+    $productListUl.append($newProduct);
+  }
+
+  // const $productsList = document.getElementById("productsList");
+  document.getElementById("productsList").append($productListUl);
+}
+
+function clearProductList() {
+  // TODO:CHECK
+  const $productListUl = document.getElementById("productListUl");
+  console.log($productListUl, 14);
+
+  // $productListUl.innerHTML = "";
+
+  while ($productListUl.firstChild) {
+    $productListUl.removeChild($productListUl.firstChild);
+  }
+  console.log("productlist was clear");
+  // debugger;
 }
 // ***********
 // main functions:
 // ***********
+
+// function createProductLi1(product, productIdNumber) {
+//   const $newProduct = elementGenerator(
+//     "li",
+//     `${product?.prdName}`,
+//     "productLi",
+//     product?.prdName
+//   );
+//   $newProduct.dataset.productId = productIdNumber;
+//   const $deleteButton = buttonGenerator(
+//     deleteProductFromUl,
+//     "X",
+//     `deleteProductButton_${productIdNumber}`,
+//     "deleteProductButton"
+//   );
+//   $deleteButton.dataset.productId = productIdNumber;
+//   $newProduct.append($deleteButton);
+//   console.log($newProduct, 11);
+//   return $newProduct;
+// }
+
+function createProductLi(product, productIdNumber) {
+  const $newProduct = elementGenerator(
+    "p",
+    `${product?.prdName}`,
+    "productP",
+    product?.prdName
+  );
+  $newProduct.dataset.productId = productIdNumber;
+  const $deleteButton = buttonGenerator(
+    deleteProductFromUl,
+    "X",
+    `deleteProductButton_${productIdNumber}`,
+    "deleteProductButton"
+  );
+  $deleteButton.dataset.productId = productIdNumber;
+  const $newProductDiv = elementGenerator(
+    "div",
+    `${product?.prdName}Div`,
+    "productDiv"
+  );
+  $newProductDiv.append($newProduct, $deleteButton);
+  const $newProductLi = elementGenerator(
+    "li",
+    `${product?.prdName}Li`,
+    "productLi"
+  );
+  $newProductLi.dataset.productId = productIdNumber;
+
+  $newProductLi.append($newProductDiv);
+  $newProductLi.addEventListener("click", refreshShownProductDetails);
+  // console.log($newProduct, 11);
+  return $newProductLi;
+}
+
+function deleteProductFromUl(e) {
+  const productIdNum = e.target.dataset.productId;
+  console.log(productIdNum, e.target);
+  deleteProductFromList(productIdNum);
+  refreshProductsList();
+  // TODO: finish
+}
 
 function addProductToList() {
   //   TODO: check using form to get value and delete
@@ -151,16 +272,46 @@ function addProductToList() {
   productList.push(newProduct);
 
   console.log("product was added", newProduct);
-  console.log($nameInput);
-  //   TODO: add single item to list instead of create product list
-  createProductsList();
+
+  const $newProduct = createProductLi(newProduct, productList.length - 1);
+  document.getElementById("productListUl").append($newProduct);
   clearInputs();
+}
+
+function showProductDetails(productIdNum) {
+  const productDetails = productList[productIdNum];
+  for (let productType of INPUT_TYPES) {
+    // console.log(
+    //   `product${
+    //     productType.charAt(0).toUpperCase() + productType.substring(1)
+    //   }`,
+    //   document.getElementById(
+    //     `product${
+    //       productType.charAt(0).toUpperCase() + productType.substring(1)
+    //     }`
+    //   ),
+    //   16
+    // );
+    console.log(productDetails[productType], 17, productType);
+
+    document.getElementById(
+      `product${productType.charAt(0).toUpperCase() + productType.substring(1)}`
+    ).innerHTML = productDetails[productType];
+  }
+}
+
+function refreshShownProductDetails(e) {
+  const productIdNum = e.target.dataset.productId;
+
+  showProductDetails(productIdNum);
 }
 
 // ***********
 // side functions:
 // ***********
-
+function deleteProductFromList(productId) {
+  productList.splice(productId, productList.length > 0 ? 1 : 0);
+}
 function clearInputs() {
   const inputsToClear = document.getElementsByClassName("productInput");
   for (let $input of inputsToClear) {
